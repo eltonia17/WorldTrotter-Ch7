b78a576304cc712ff66d96cf3f320e02fc38ba7d
Leonard <esl107@miami.edu>
2025-10-05 16:41:53 -0400

    Chapter 7 Bronze challenge (Spanish + French (with quiz add on))

--- WorldTrotter/QuizViewController.swift
+++ WorldTrotter/QuizViewController.swift
@@ -8,43 +8,38 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-    
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
-    
+
+    // Use localized keys instead of hardcoded English
     let questions: [String] = [
-            "What is 7+7?",
-            "What is the capital of Vermont?",
-            "What is cognac made from?"
-        ]
+        NSLocalizedString("quiz.q1", comment: "Question 1"),
+        NSLocalizedString("quiz.q2", comment: "Question 2"),
+        NSLocalizedString("quiz.q3", comment: "Question 3")
+    ]
+
     let answers: [String] = [
-            "14",
-            "Montpelier",
-            "Grapes"
-        ]
+        NSLocalizedString("quiz.a1", comment: "Answer 1"),
+        NSLocalizedString("quiz.a2", comment: "Answer 2"),
+        NSLocalizedString("quiz.a3", comment: "Answer 3")
+    ]
+
     var currentQuestionIndex: Int = 0
 
     @IBAction func showNextQuestion(_ sender: UIButton) {
         currentQuestionIndex += 1
-        if currentQuestionIndex == questions.count {
-                currentQuestionIndex = 0
-            }
+        if currentQuestionIndex == questions.count { currentQuestionIndex = 0 }
 
-            let question: String = questions[currentQuestionIndex]
-            questionLabel.text = question
-            answerLabel.text = "???"
+        questionLabel.text = questions[currentQuestionIndex]
+        answerLabel.text = "???"  
+    }
 
-        }
-        @IBAction func showAnswer(_ sender: UIButton) {
-            let answer: String = answers[currentQuestionIndex]
-            answerLabel.text = answer
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = answers[currentQuestionIndex]
+    }
 
-        }
-    
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            questionLabel.text = questions[currentQuestionIndex]
-        }
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
 }
-
